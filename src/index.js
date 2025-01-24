fd50a6117c7288d7622080d488d10dc1dcc83fd1
jackcorless91 <146907668+jackcorless91@users.noreply.github.com>
2025-01-25 10:01:36 +1100

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// import express
+const express = require("express")
+const mongoose = require("mongoose")
+
+const postRouter = require("./routes/postRoutes")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({
+        data: "Hello World!!"
+    })
+})
+
+app.get("/hello", (req, res) => {
+    res.json({
+        data: "Another route named hello"
+    })
+})
+
+app.use("/posts", postRouter)
+
+app.listen(3000, async () => {
+    console.log("Server started")
+    // mongoose.connect("mongodb://127.0.0.1:27017/blog_db").then(() => {
+    //     console.log("Database connected")
+    // })
+    await mongoose.connect("mongodb://127.0.0.1:27017/blog_db")
+    console.log("Database connected")
+})
